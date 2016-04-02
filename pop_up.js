daf416b8881c49313f6b7e8a758d06b24024c6a6
VPoint <VPoint@users.noreply.github.com>
2016-04-02 09:34:06 -0400

    Add files
    This is necessary for javascript injection purposes.

---
+++ pop_up.js
@@ -0,0 +1,45 @@
+// Call on all URLS.
+alert("Hello from your Chrome extension!");
+
+var newScript=document.createElement('script');
+newScript.innerHtml = "
+var start = setInterval(myTimer, 3000);
+
+var length = 200;
+var hieght = 150;
+var left = 0;
+var top = 0;
+
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+function myTimer() {
+    if(left + length >= w){
+	if(top + height >= h){
+	   clearInterval();
+	}
+       top = top + height;
+    }
+   createDiv();
+}
+
+function createDiv(){
+//var node = document.getElementById('demo');
+var div = document.createElement("div");
+div.style.width = "200px";
+div.style.height = "100px";
+div.style.background = "red";
+div.style.color = "blue";
+div.innerHTML = "Hello";
+div.style.position = "absolute";
+div.style.left = "0px";
+div.style.top = "0px";
+
+body.appendChild(div);
+}
+";
+
+var head=document.getElementsByTagName('head')[0]...;
+head.appendChild(script);
+
+// 5 minutes = 300000 milliseconds
